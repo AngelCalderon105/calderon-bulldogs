8ed5d4aca2407685bf2a677f2f9d22eed5695b52
csa67 <saiaarathy@gmail.com>
2024-08-19 16:19:04 -0400

    Splide and Image carousel

--- src/app/_components/MultipleFileUpload.tsx
+++ src/app/_components/MultipleFileUpload.tsx
@@ -1,6 +1,7 @@
 "use client"
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { useState, ChangeEvent } from "react";
-
+import '@splidejs/splide/dist/css/splide.min.css';
 
 const MultipleFileUpload: React.FC = () => {
     const [files, setFiles] = useState<File[]>([]);
@@ -34,6 +35,33 @@ const MultipleFileUpload: React.FC = () => {
                 onClick={handleUpload}
                 className="mt-4 p-4 bg-blue-600 text-white rounded">Upload</button>
 
+            {
+                 filePreviews.length > 0 && (
+                    <div className="mt-4">
+          <h3 className="font-medium mb-2">Selected files:</h3>
+          <Splide
+            options={{
+              perPage: 1,
+              gap: '2rem',
+              pagination: false,
+              arrows: true,
+              drag: 'free',
+            }}
+            className="splide"
+          >
+            {filePreviews.map((preview, index) => (
+              <SplideSlide key={index}>
+                <img
+                  src={preview}
+                  alt={`File preview ${index + 1}`}
+                  className="w-full h-40 object-cover rounded-lg"
+                />
+              </SplideSlide>
+            ))}
+          </Splide>
+          </div>
+                 )
+            }
         </div>
     );
 }
