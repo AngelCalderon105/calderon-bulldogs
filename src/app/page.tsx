b50e60ec728f2810f5755fac1ff23f4728a23c6e
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2025-03-27 14:13:30 -0700

    Updating Featured Puppies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,43 @@
-import GalleryView from "~/app/_components/GalleryView"
+import GalleryView from "~/app/_components/GalleryView";
 import FaqView from "./_components/FaqView";
-import TestimonialView from "./_components/TestimonialView"
-import ContactView from "./_components/ContactView"
+import TestimonialView from "./_components/TestimonialView";
+import ContactView from "./_components/ContactView";
 import PuppyManagement from "./_components/PuppyManagement";
 import EventView from "./_components/EventView";
 import StudServiceCard from "./_components/StudService";
 import HowItWorks from "./_components/Howitworks";
-import Footer from "./_components/Footer"
+import Footer from "./_components/Footer";
 import HomeView from "./_components/HomeView";
 export default async function Home() {
-
   return (
     <>
-  
-    <div id="home">
-      <HomeView isAdmin={false}/>
-    </div>
-    
-    <div id="available_puppies">
-      <PuppyManagement isAdmin={false} />
-    </div>
-    
-    <div id="stud_service">
-      <StudServiceCard /> 
-    </div>
+      <div id="home">
+        <HomeView isAdmin={false} />
+      </div>
 
-    <div id="faq" className="mx-6 sm:mx-10 md:mx-16 lg:mx-20 xl:mx-32 2xl:mx-40">
-      <FaqView isAdmin={false}/>
-    </div>
+      <div id="available_puppies" className="mt-10">
+        {/* <PuppyManagement isAdmin={false} /> */}
+      </div>
 
-    <EventView isAdmin={false}/>
+      <div id="stud_service">
+        <StudServiceCard />
+      </div>
+
+      <div
+        id="faq"
+        className="mx-6 sm:mx-10 md:mx-16 lg:mx-20 xl:mx-32 2xl:mx-40"
+      >
+        <FaqView isAdmin={false} />
+      </div>
+
+      <EventView isAdmin={false} />
       <div id="contact">
         <ContactView isAdmin={false} />
       </div>
 
-  <div className="m-2 my-20 lg:mt-10 lg:mb-20 lg:mx-10">
-  <Footer />
- </div>
-     </>
+      <div className="m-2 my-20 lg:mx-10 lg:mb-20 lg:mt-10">
+        <Footer />
+      </div>
+    </>
   );
 }
